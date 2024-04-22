0a49ea2deac8aed7dce2d7aa54fb7cede4502c23
KJ <106093018+itsherkj@users.noreply.github.com>
2024-04-22 13:13:14 +0800

    Files

---
+++ Snake Game/src/Frame.java
@@ -0,0 +1,25 @@
+import java.awt.Color;
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
+public class Frame extends JFrame {
+
+    Frame() {
+
+        this.setTitle("Snake Game"); // title of the game on the frame
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // exit out of appliaction
+        this.setResizable(false); // prevent frame from resizing
+        this.setSize(450, 450); // sets x-axis and y-axis of the frame
+        this.setVisible(true); // makes the frame visible
+        // this.setLayout(null);
+        // this.add(l);
+        // this.pack();
+
+        ImageIcon i = new ImageIcon("logo.png"); // create an image icon
+        this.setIconImage(i.getImage()); // change icon of frame
+        this.getContentPane().setBackground(new Color(6, 137, 2)); // change color of the background
+    }
+
+}
