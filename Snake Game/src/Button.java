df7bf6576db30f3e98e23303a87fd98c80215ec8
cipher <cipher>
2024-04-28 09:27:36 +0800

    new file:   Snake Game/src/Button.java

---
+++ Snake Game/src/Button.java
@@ -0,0 +1,32 @@
+/*import java.awt.*;
+
+
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Button extends JFrame implements addActionListener{  
+    
+    button = new JButton();
+    button.setBounds(100, 100, 250, 100);
+    button.addActionListener(this);
+    button.setText("START");
+    
+    button.setFocusable(false);
+    button.setHorizontalTextPosition(JButton.CENTER);
+    button.setVerticalTextPosition(JButton.BOTTOM);
+    button.setFont(new Font("Comic Sans",Font.BOLD,25));
+    button.setForeground(Color.cyan);
+    button.setBackground(Color.lightGray);
+    button.setBorder(BorderFactory.createEtchedBorder());
+    
+void actionPerformed(ActionEvent e) {
+    Object button;
+    if(e.getSource()==button) {
+        System.out.println("play");
+        ((Component) button).setEnabled(false);
+        Window l;
+        l.setVisible(true);
+        }	
+    }
+}*/
+
