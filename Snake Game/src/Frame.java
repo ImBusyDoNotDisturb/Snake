831ada72117c433cbfeb871b34447ceb8ec29382
yana <cipher>
2024-04-30 11:24:30 +0800

    deleted:    Snake Game/src/Button.java 	modified:   Snake Game/src/Frame.java

--- Snake Game/src/Frame.java
+++ Snake Game/src/Frame.java
@@ -1,22 +1,51 @@
+import java.awt.BorderLayout;
 import java.awt.Color;
+
+import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class Frame extends JFrame {
-
     Frame() {
 
         JFrame f = new JFrame();
+        JButton button = new JButton();
+        JButton button2 = new JButton();
+        button.setBounds(165, 250, 100, 30);
+        button2.setBounds(165, 290, 100, 30);
+        //button.addActionListener(e -> System.out.println());
+        //button2.addActionListener(e -> System.out.println());
+        button.setText("START");
+        button2.setText("EXIT");
+        button.setFocusable(false);
+        button2.setFocusable(false);
+        //button.setHorizontalTextPosition(JButton.CENTER);
+        //button.setVerticalTextPosition(JButton.BOTTOM);
+        button.setFont(new Font("Monospaced", Font.BOLD, 20));
+        button2.setFont(new Font("Monospaced", Font.BOLD, 20));
+        Color customColor = new Color(0, 204, 0);
+        button.setBackground(customColor);
+        button2.setBackground(customColor);
+        button.setOpaque(true);
+        button2.setOpaque(true);
+        button.setBorderPainted(false);
+        button2.setBorderPainted(false);
+        
+        
+        
 
         this.setTitle("Snake Game"); // title of the game on the frame
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // exit out of appliaction
         this.setResizable(false); // prevent frame from resizing
         this.setSize(450, 450); // sets x-axis and y-axis of the frame
-
         this.setLocationRelativeTo(null);
-        // this.setLayout(null);
+        //this.setLayout(null);
+        //this.setVisible(true);
         // this.add(l);
         // this.pack();
 
@@ -28,21 +57,32 @@ public class Frame extends JFrame {
         ImageIcon image = new ImageIcon("Snake Game/src/images here/logo.png");
         // Border b = BorderFactory.createLineBorder(Color.green,3);
 
-        l.setText("Snake"); // set text of label
+        l.setText("SNAKE"); // set text of label
         l.setIcon(image);
         l.setHorizontalTextPosition(JLabel.CENTER); // set text left, center, right of image
         l.setVerticalTextPosition(JLabel.TOP); // set text top, center, botton of image
         l.setForeground(new Color(0, 0, 0)); // set font color of text
-        l.setFont(new Font("Ariel", Font.BOLD, 50)); // set font of text
-        l.setIconTextGap(-50); // set gap of text to image
+        l.setFont(new Font("Serif", Font.BOLD, 100)); // set font of text
+        l.setIconTextGap(-40); // set gap of text to image
         l.setBackground(new Color(11, 148, 54));
         l.setOpaque(true);
         // l.setBorder(border); // sets border of label(not image and text)
         l.setVerticalAlignment(JLabel.CENTER); // set vertical position of icon and text within the label
         l.setHorizontalAlignment(JLabel.CENTER); // set horizontal position of icon and text within the label
         l.setBounds(0, 0, 450, 450);
+        this.add(button);
+        this.add(button2);
         this.add(l);
         this.setVisible(true); // makes the frame visible
+
+        /*@Override
+        public void actionPerformed(ActionEvent e){
+            if(e.getSource()==button){
+                System.out.println();
+            }
+        }*/
+
+        
 
     }
 
