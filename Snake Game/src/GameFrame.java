24dc778e44528dd14360736b81efe300e00b595a
iana <104040911+ImBusyDoNotDisturb@users.noreply.github.com>
2024-05-09 22:28:58 +0800

    Update GameFrame.java

--- Snake Game/src/GameFrame.java
+++ Snake Game/src/GameFrame.java
@@ -4,19 +4,8 @@ import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
 public class GameFrame extends JFrame{
-
-    private static final long serialVersionUID = 1L;
-
-	static final int cols = 8;
-	static final int rows = 8;
-
-	static final int originX = 23;
-	static final int originY = 37;
-	static final int cellSide = 60;
-
-	private int i;
-
     public GameFrame(){
+
         this.setTitle("Snake Game"); // title of the game on the frame
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // exit out of appliaction
         this.setResizable(false); // prevent frame from resizing
@@ -28,6 +17,7 @@ public class GameFrame extends JFrame{
 
 
         setVisible(true);
+
     }
 
 }
