b4f2edad639c8574534009549b6a6f3430e9e270
iana <104040911+ImBusyDoNotDisturb@users.noreply.github.com>
2024-05-13 22:29:15 +0800

    modified:   Snake Game/src/Panel.java

--- Snake Game/src/Panel.java
+++ Snake Game/src/Panel.java
@@ -11,16 +11,16 @@ public class Panel extends JPanel implements ActionListener {
 
     static final int SCREEN_WIDTH = 450;
     static final int SCREEN_HEIGHT = 450;
-    static final int UNIT_SIZE = 20;
+    static final int UNIT_SIZE = 20; //size of square
     static final int GAME_UNITS = (SCREEN_WIDTH*SCREEN_HEIGHT)/UNIT_SIZE;
-    static final int DELAY = 50;
+    static final int DELAY = 75; // speed the lower the faster
     final int x[] = new int[GAME_UNITS];
     final int y[] = new int[GAME_UNITS];
     int bodyParts = 6;
     int appleEaten;
     int appleX;
     int appleY;
-    char direction = 'R';
+    char direction = 'D';
     boolean running = false;
     Timer timer;
     Random random;
@@ -49,17 +49,17 @@ public class Panel extends JPanel implements ActionListener {
 
     public void draw(Graphics g){
       if(running){
-        /*for (int i = 0; i<SCREEN_HEIGHT/UNIT_SIZE; i++){
+        for (int i = 0; i<SCREEN_HEIGHT/UNIT_SIZE; i++){
           g.drawLine(i*UNIT_SIZE, 0, i*UNIT_SIZE, SCREEN_HEIGHT);
           g.drawLine(0, i*UNIT_SIZE, SCREEN_WIDTH, i*UNIT_SIZE);
-        }*/
+        }
         g.setColor(Color.red);
         g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
 
           for(int i = 0; i<bodyParts; i++){
-          if(i == 0){
-            g.setColor(Color.BLUE);
-            g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+            if(i == 0){
+              g.setColor(Color.BLUE);
+              g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
           }
           else{
             g.setColor(Color.ORANGE);
